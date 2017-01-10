a0171b7d268fa6387758917cb24fc1bcaf6d0391
Syanmil <syanmil@yahoo.com>
2017-01-10 21:48:57 +0700

    Interface hospital tapi command masih belum jalan

--- hospital.js
+++ hospital.js
@@ -3,18 +3,19 @@ const faker = require('faker')
 const readline = require('readline');
 const fs = require('fs')
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 class Interface{
   constructor(){
     this.user = []
     this.pass = []
+    this.active = {}
   }
   header(){
-    console.log(`Welcome to ${rumahSehat.name} Hospital`);
+    console.log(`Welcome to ${rumahSehat.name} ${rumahSehat.location} Hospital`);
     console.log("-------------------------------------------");
   }
   login(){
@@ -23,11 +24,40 @@ class Interface{
       rl.question('Please enter your password: ', (password) => {
         this.user.push(`${username}`);
         this.pass.push(`${password}`);
-        console.log(this.user, this.pass);
+        if (this.cek()){
+          this.mainPage()
+          this.option()
+        } else {
+          console.log("user atau password anda salah")
+          rl.close()
+        }
         rl.close()
       });
     });
   };
+  cek(){
+    for (var i = 0; i <rumahSehat.employeesList.length; i++){
+      if (rumahSehat.employeesList[i].username == this.user[0]){
+        if (rumahSehat.employeesList[i].password == this.pass[0]){
+          this.active = rumahSehat.employeesList[i]
+          return true
+        }
+      }
+    }
+    return false
+  };
+  mainPage(){
+    console.log("-----------------------------------");
+    console.log(`Selamat datang, ${this.active.name}. Your access level is ${this.active.position}`);
+    console.log("-----------------------------------");
+  }
+  option(){
+    if (this.active.position === "dokter"){
+      console.log(`Apa yang ingin anda lakukan?\n Options:\n 1 -- list_patients\n 2 -- view_records <patient_id>\n 3 -- add_record <patient_id> \n 4 -- remove_record <patient_id><record_id>\n`)
+    } else if(this.active.position === "administrator"){
+      console.log(`Apa yang ingin anda lakukan?\n Options:\n 1 -- list_karyawan\n 2 -- tambah_karyawan\n 3 -- pecat_karyawan\n 4 -- tambah_pasien\n`)
+    };
+  }
 }
 
 class Hospital {
@@ -61,9 +91,9 @@ class Employee {
   }
 }
 
-var rumahSehat = new Hospital('Rumah Sehat', 'Pondok Indah', 15, 70);
-console.log(rumahSehat.addEmployee(faker.name.findName(), "admin", faker.name.firstName(), "12345"))
-// console.log(rumahSehat);
-// var menu = new Interface();
-// menu.header()
-// menu.login()
+var rumahSehat = new Hospital(faker.company.companyName(), faker.address.city(), 15, 70);
+rumahSehat.addEmployee(faker.name.findName(), "administrator", "admin", "12345")
+rumahSehat.addEmployee(faker.name.findName(), "dokter", "doctor", "54321")
+var menu = new Interface();
+menu.header()
+menu.login()
