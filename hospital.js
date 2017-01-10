7a0f406670a154aecb8b5c82535d9c719ac0d606
Syanmil <syanmil@yahoo.com>
2017-01-10 16:49:22 +0700

    initialization readline for autentification

--- hospital.js
+++ hospital.js
@@ -1,9 +1,46 @@
+"use strict"
+const faker = require('faker')
+const readline = require('readline');
+const fs = require('fs')
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+class Interface{
+  constructor(){
+    this.user = []
+    this.pass = []
+  }
+  header(){
+    console.log(`Welcome to ${rumahSehat.name} Hospital`);
+    console.log("-------------------------------------------");
+  }
+  login(){
+    rl.prompt()
+    rl.question('Please enter your username: ', (username) => {
+      rl.question('Please enter your password: ', (password) => {
+        this.user.push(`${username}`);
+        this.pass.push(`${password}`);
+        console.log(this.user, this.pass);
+        rl.close()
+      });
+    });
+  };
+}
+
 class Hospital {
   constructor(name, location, employees, patients) {
     this.name = name
     this.employees = employees
     this.patients = patients
     this.location = location
+    this.employeesList = []
+  }
+  addEmployee(name, position, username, password){
+    this.employeesList.push(new Employee(name, position, username, password))
+    return this.employeesList
   }
 }
 
@@ -23,3 +60,10 @@ class Employee {
     this.password = password
   }
 }
+
+var rumahSehat = new Hospital('Rumah Sehat', 'Pondok Indah', 15, 70);
+console.log(rumahSehat.addEmployee(faker.name.findName(), "admin", faker.name.firstName(), "12345"))
+// console.log(rumahSehat);
+// var menu = new Interface();
+// menu.header()
+// menu.login()
