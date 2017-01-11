555ae1ecfe79e93a7355a628c65569b77663e0f7
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-01-11 18:39:05 +0700

    WIP 1

--- hospital.js
+++ hospital.js
@@ -1,3 +1,5 @@
+const readline = require('readline');
+
 class Hospital {
   constructor(name, location, employees, patients) {
     this.name = name
@@ -13,6 +15,13 @@ class Patient {
     this.name = name
     this.diagnosis = diagnosis
   }
+
+  getName(){
+    return this.name
+  }
+  getID(){
+    return this.id
+  }
 }
 
 class Employee {
@@ -23,3 +32,36 @@ class Employee {
     this.password = password
   }
 }
+
+let john = new Patient(1801403324, 'john', 'cancer')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Welcome to Mistic Hospital\n------------------------------- \nPlease Enter your username\n>'
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+  switch(line.trim()) {
+    case john.getName():
+      rl.setPrompt('Please enter your password\n>');
+      rl.prompt();
+        switch(line.trim()) {
+          case john.id():
+
+          break;
+        }
+
+      break;
+    default:
+      console.log('Wrong username!');
+      rl.close();
+      break;
+  }
+})
+// .on('close', () => {
+//   console.log('Have a great day!');
+//   process.exit(0);
+// });
