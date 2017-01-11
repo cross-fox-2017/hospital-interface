4270f53a322d93d51fd0b4e9a89aec67e1ef9c0d
Your Name <name@example.com>
2017-01-11 17:01:01 +0700

    Create Class Hospital

--- hospital.js
+++ hospital.js
@@ -5,3 +5,18 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+class Hospital {
+    constructor(namaRS, lokasiRS) {
+        this.namaRS = namaRS
+        this.lokasiRS = lokasiRS
+        this.namaKaryawan = ["OmTelolet", "OmGoogle", "OmOBom", "Dude","omDokterOm","Siska"]
+        this.tipeKaryawan = ["admin", "admin", "ob", "ob","dokter","receptionist"]
+        this.userKaryawan = ["admin1", "admin2", "ob1", "ob2","dokter1","rs1"]
+        this.passwordKaryawan = ["adminSatu", "adminDua", "obSatu", "obDua","dokterSatu","rsSatu"]
+        this.namaPasien = ["budi", "budo", "bubu"]
+        this.penyakitPasien = ["Sakit Hati", "Sakit Jiwa", "Sakit Jantung"]
+        this.userID
+    }
+  
+}
