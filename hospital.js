e087790d373ab87059d5de2d74fc30916f882685
didietsuryadi <didietsuryadi@gmail.com>
2017-01-23 20:54:29 +0700

    finish

--- hospital.js
+++ hospital.js
@@ -1,25 +1,176 @@
+"use strict"
+
+const readline = require('readline');
+const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+});
+
 class Hospital {
-  constructor(name, location, employees, patients) {
-    this.name = name
-    this.employees = employees
-    this.patients = patients
-    this.location = location
+    constructor(name, location) {
+        this.name = name
+        this.location = location
+        this.employees = ['Andi','Rian','Agung','Ari','Patient']
+        this.username = ['andi','rian','agung','ari','patient']
+        this.position = ['Doctor','Admin','Receptionist','Officeboy','Patient']
+        this.patients = [{id:1, name:"Kamil", diagnose:"flu"},{id:2, name:"Ahmad", diagnose:"batuk"}]
+        this.id = 0
   }
+
+  header(){
+      console.log("\n\n================================================");
+      console.log(`Welcome to ${this.name} Hospital ${this.location}`);
+      console.log("================================================");
+      this.login()
+  }
+
+  login(){
+      rl.question('\n\nPlease enter your username (hint : andi / rian / agung / ari / patient)\nAnswer : ', (answer) => {
+          if (this.username.indexOf(answer) !== -1 ) {
+              this.id = this.username.indexOf(answer)
+              this.password();
+          }else {
+            console.log('\n\n Try again!. Invalid username !.\n');
+            this.login();
+          }
+      });
+  }
+
+  password(){
+      rl.question('\n\nPlease enter your Password (use your username)\nAnswer : ', (answer) => {
+          if (answer === this.username[this.id]) {
+              this.welcome()
+          }else {
+            console.log('\n\nInvalid password. Try again!\n');
+            this.password();
+          }
+      });
+  }
+
+  welcome(){
+      console.log("\n\n================================================");
+      console.log(`Welcome ${this.employees[this.id]}. Your access level is : ${this.position[this.id]}`)
+
+      this.home()
+  }
+
+  home(){
+    console.log("\n================================================");
+    console.log('What would you like to do?');
+    console.log('Option : ');
+    console.log('-- list_patients');
+    console.log('-- view_records');
+    console.log('-- add_records');
+    console.log('-- remove_records');
+
+    this.option()
+  }
+
+  list_patients(){
+      if (this.patients.length == 0) {
+          console.log("\nThere is no patient listed");
+          this.home()
+      }
+      else if (this.id == 4){
+          console.log("\n**You can only access your own medical report**\n");
+          this.view_records()
+      }
+      else if (this.id == 3) {
+          console.log("\nYou can only access id and patient's name");
+          console.log('\n=======================\nPatients List : \n======================\n\nID     Name');
+          for (var i = 0; i < this.patients.length; i++) {
+              console.log(`${this.patients[i].id}      ${this.patients[i].name}`);
+          }
+          this.home()
+      }else {
+          console.log('\n\n=======================\nPatients List : \n======================\n\nID     Name     Diagnose');
+          for (var i = 0; i < this.patients.length; i++) {
+              console.log(`${this.patients[i].id}      ${this.patients[i].name}   ${this.patients[i].diagnose}`);
+          }
+          this.home()
+      }
+      console.log('\n\n');
+  }
+
+  view_records(){
+   rl.question('\nEnter patient ID: (type exit to back to home page)', (answer) => {
+     if (answer > 0 && answer <= this.patients.length) {
+       if (this.id == 3) {
+         console.log(`ID : ${this.patients[answer-1].id}\nName : ${this.patients[answer-1].name}\n`);
+         this.view_records()
+       }
+       else {
+         console.log(`ID : ${this.patients[answer-1].id}\nName : ${this.patients[answer-1].name}\nDiagnose : ${this.patients[answer-1].diagnose}`);
+         this.view_records()
+       }
+     }
+     else if (answer == 'exit') {
+        this.home()
+     }else {
+       console.log('Record not found. Please make sure if your ID has been registered');
+       this.view_records()
+     }
+   });
+ }
+
+ add_records(){
+    if (this.id == 3 || this.id == 4) {
+        console.log('Access denied! Only Doctors, Admins, and Receptionists can add new patient');
+        this.home()
+    }else {
+      rl.question('\nEnter patient\'s name & diagnose (ex: john,diarrhea) : ', (answer) => {
+        let input = answer.split(',');
+        this.patients.push({id: this.patients.length + 1, name: input[0], diagnose: input[1]})
+        console.log('\nNew patient has been recorded')
+        this.home()
+      });
+    }
+  }
+
+  remove_records(){
+     if (this.id == 3 || this.id == 4) {
+         console.log('\nAccess denied! Only Doctors, Admins, and Receptionists can remove patient');
+         this.home()
+     }else {
+       rl.question('\nEnter patient\'s ID to remove : ', (answer) => {
+         this.patients.splice(answer-1,1)
+         console.log(`\nPatient with ID : ${answer} has been removed`)
+         this.home()
+       });
+     }
+   }
+
+  option(){
+      rl.question('\nEnter option or exit to logout:\ninput : ', (answer) => {
+          switch (answer) {
+              case 'list_patients':
+                  this.list_patients()
+                  break;
+
+              case 'view_records':
+                  this.view_records()
+                  break;
+
+              case 'add_records':
+                  this.add_records()
+                  break;
+
+              case 'remove_records':
+                  this.remove_records()
+                  break;
+
+              case 'exit':
+                  this.header()
+                  break;
+
+            default:
+                console.log("\nincorrect option format");
+                this.option()
+          }
+      });
+  }
+
 }
 
-class Patient {
-  constructor(id, name, diagnosis) {
-    this.id = id
-    this.name = name
-    this.diagnosis = diagnosis
-  }
-}
-
-class Employee {
-  constructor(name, position, username, password) {
-    this.name = name
-    this.position = position
-    this.username = username
-    this.password = password
-  }
-}
+let hospital = new Hospital('Didit','Jakarta')
+hospital.header()
